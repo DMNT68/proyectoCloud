81df401240758ba7cd6f53ec98da8b92785bd8e1
DMNT68 <andressalgado41@gmail.com>
2017-12-10 21:48:02 -0500

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,4 +1,4 @@
 <?php 
-	$conn=pg_connect("dbname=user_db user=postgres password=111111")or die("Cannnot db,please check your connection string");
+	$conn=pg_connect("host=postgres://mnbfqwhacjnavr:8d1307c043b491e20d8f8e4fda3abc6b31c8191c4259277a412d17ed593316af@ec2-50-17-203-84.compute-1.amazonaws.com:5432/daer76rdjif7l7 port=5432 dbname=daer76rdjif7l7 user=mnbfqwhacjnavr password=8d1307c043b491e20d8f8e4fda3abc6b31c8191c4259277a412d17ed593316af")or die("Cannnot db,please check your connection string");
 ?>
 
